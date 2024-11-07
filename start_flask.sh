2cd03543db8eea18b09aea1bbfee0139021d6045
halop <halop8@gmail.com>
2024-11-07 08:44:03 +0100

    start script

--- start_flask.sh
+++ start_flask.sh
@@ -2,4 +2,5 @@
 
 cd /home/<user>/switch-lan-play-raspi
 source myenv/bin/activate
+sudo setcap cap_net_raw,cap_net_admin=eip /home/<user>/switch-lan-play-raspi/lan-play
 python -m flask run --host=192.168.1.110
