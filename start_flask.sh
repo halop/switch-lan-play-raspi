2c99baf16f5b7a948c2136b77f33e3c37259c823
halop <halop8@gmail.com>
2024-11-08 19:12:53 +0100

    Update start_flask.sh

--- start_flask.sh
+++ start_flask.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/<user>/switch-lan-play-raspi
-sudo /home/<user>/switch-lan-play-raspi/myenv/bin/flask run --host <ip of raspberry>
+/home/<user>/switch-lan-play-raspi/myenv/bin/flask run --host <ip of raspberry>
